20bdd8e3776cce13a3a39e5232594090581cd6d1
EuAres56 <mitosobr@gmail.com>
2026-01-30 22:06:20 -0300

    add create_history

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ export default {
             // ===============================
             // 📊 REGISTRA NO GOOGLE SHEETS
             // ===============================
-            const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbx_JnwqQ_5EngRMRQT8mkqpqO9G8JFyfC1de3_b74hcNFa6s9AwvZVoyI2QghtB_66D/exec";
+            const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbx_JnwqQ_5EngRMRQT8mkqpqO9G8JFyfC1de3_b74hcNFa6s9AwvZVoyI2QghtB_66D/exechttps://script.google.com/macros/s/AKfycbx_JnwqQ_5EngRMRQT8mkqpqO9G8JFyfC1de3_b74hcNFa6s9AwvZVoyI2QghtB_66D/exec";
 
             // envio assíncrono (não trava o fluxo)
             fetch(GOOGLE_SHEETS_URL, {
